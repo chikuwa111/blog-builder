2e62a41cdcdf5ac4f53d929b7f91d0fe09158a43
chikuwa111 <take0shun1111@gmail.com>
2020-12-06 00:15:02 +0900

    refactor: srcを作成

---
+++ src/build/index.ts
@@ -0,0 +1,37 @@
+import fm from "front-matter";
+import fs from "fs/promises";
+import path from "path";
+
+async function isPublished(filePath: string): Promise<boolean> {
+  const data = await fs.readFile(filePath, "utf-8");
+  const content = fm(data);
+  // front matterで`published: true`となっているものだけpublishする
+  return !!(content.attributes as { published: boolean | undefined }).published;
+}
+
+export async function build() {
+  const { CONTENT_PATH, DIST_PATH } = process.env;
+  if (!CONTENT_PATH) {
+    throw new Error("process.env.CONTENT_PATH is not specified.");
+  }
+  if (!DIST_PATH) {
+    throw new Error("process.env.DIST_PATH is not specified.");
+  }
+
+  const fileNames = await fs.readdir(CONTENT_PATH);
+  await Promise.all(
+    fileNames
+      .filter((fileName) => path.extname(fileName) === ".md")
+      .map(async (fileName) => {
+        const contentPath = path.resolve(CONTENT_PATH, fileName);
+
+        const published = await isPublished(contentPath);
+        if (!published) {
+          return;
+        }
+
+        const distPath = path.resolve(DIST_PATH, fileName);
+        await fs.copyFile(contentPath, distPath);
+      })
+  );
+}
