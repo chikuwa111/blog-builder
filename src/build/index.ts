44ff28ac6d4895e7ff64a8f7b859e1fd4aa18b1a
chikuwa111 <take0shun1111@gmail.com>
2020-12-06 00:51:12 +0900

    feat: ファイルの削除機能を追加

--- src/build/index.ts
+++ src/build/index.ts
@@ -2,11 +2,35 @@ import fm from "front-matter";
 import fs from "fs/promises";
 import path from "path";
 
-async function isPublished(filePath: string): Promise<boolean> {
-  const data = await fs.readFile(filePath, "utf-8");
-  const content = fm(data);
-  // front matterで`published: true`となっているものだけpublishする
-  return !!(content.attributes as { published: boolean | undefined }).published;
+type MyFrontMatterAttributes = {
+  published: boolean | undefined;
+};
+
+/** front matter で `published: true` となっているものだけ publish する */
+function isPublished(attributes: MyFrontMatterAttributes): boolean {
+  return !!attributes.published;
+}
+
+function filterMarkdown(fileName: string): boolean {
+  return path.extname(fileName) === ".md";
+}
+
+/** DIST_PATH にあって CONTENT_PATH からなくなっているファイルがあったら DIST_PATH からも削除する */
+async function syncDeletedFiles(
+  distFileNames: string[],
+  contentFileNames: string[],
+  distPath: string
+): Promise<void> {
+  const deletedFiles = distFileNames.filter(
+    (fileName) => !contentFileNames.includes(fileName)
+  );
+
+  await Promise.all(
+    deletedFiles.map(async (fileName) => {
+      const filePath = path.resolve(distPath, fileName);
+      await fs.rm(filePath);
+    })
+  );
 }
 
 export async function build() {
@@ -18,20 +42,31 @@ export async function build() {
     throw new Error("process.env.DIST_PATH is not specified.");
   }
 
-  const fileNames = await fs.readdir(CONTENT_PATH);
+  const distFileNames = (await fs.readdir(DIST_PATH)).filter(filterMarkdown);
+  const contentFileNames = (await fs.readdir(CONTENT_PATH)).filter(
+    filterMarkdown
+  );
+
+  await syncDeletedFiles(distFileNames, contentFileNames, DIST_PATH);
+
   await Promise.all(
-    fileNames
-      .filter((fileName) => path.extname(fileName) === ".md")
-      .map(async (fileName) => {
-        const contentPath = path.resolve(CONTENT_PATH, fileName);
+    contentFileNames.map(async (fileName) => {
+      const contentPath = path.resolve(CONTENT_PATH, fileName);
+      const distPath = path.resolve(DIST_PATH, fileName);
 
-        const published = await isPublished(contentPath);
-        if (!published) {
-          return;
+      const data = await fs.readFile(contentPath, "utf-8");
+      const { attributes } = fm<MyFrontMatterAttributes>(data);
+
+      const published = isPublished(attributes);
+      if (!published) {
+        // unpublished になったファイルは DIST_PATH から削除する
+        if (distFileNames.includes(fileName)) {
+          await fs.rm(distPath);
         }
+        return;
+      }
 
-        const distPath = path.resolve(DIST_PATH, fileName);
-        await fs.copyFile(contentPath, distPath);
-      })
+      await fs.copyFile(contentPath, distPath);
+    })
   );
 }
