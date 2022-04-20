fa9ebaf5f321407c94738c931015632021696d66
chikuwa111 <take0shun1111@gmail.com>
2022-04-21 08:55:29 +0900

    Add update-wikilink script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 cd `dirname $0`
-npm run build && npx foam-cli janitor -w $(npm run --silent get-dest-path)
+npm run build && npm run update-wikilink -- $(npm run --silent get-dest-path)
