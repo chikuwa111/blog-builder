01f83259ecd3799792cc2ba56e06f8f4a4066bd0
chikuwa111 <take0shun1111@gmail.com>
2022-04-21 02:32:40 +0900

    Use foam-cli with w option

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 cd `dirname $0`
-npm run build && npx foam-cli janitor $(npm run --silent get-dest-path)
+npm run build && npx foam-cli janitor -w $(npm run --silent get-dest-path)
